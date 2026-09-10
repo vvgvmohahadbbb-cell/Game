55bd7b37ab69d5b34446f93d9cc5be519f612fcb
مصطفى <test@example.com>
2026-09-10 13:19:59 +0300

    إصلاح استيراد Button الناقص

--- app/src/main/java/com/ishhf/fightarena/LobbyActivity.kt
+++ app/src/main/java/com/ishhf/fightarena/LobbyActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
